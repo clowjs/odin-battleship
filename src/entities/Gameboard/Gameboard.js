0339a44a055909418f583ac9c91dfa4d5fdf936e
Sergio Santos <64509249+clowjs@users.noreply.github.com>
2024-03-25 10:41:44 -0300

    feat: add basic functions to Gameboard

--- src/entities/Gameboard/Gameboard.js
+++ src/entities/Gameboard/Gameboard.js
@@ -1,17 +1,94 @@
 class Gameboard {
   constructor() {
+    // 0 = empty space
+    // 1 = ship
+    // 2 = miss
+    // 3 = hit
+    this.ships = [];
     this.board = [];
+
+    for (let i = 0; i < 10; i += 1) {
+      this.board.push(Array(10).fill(0));
+    }
   }
 
-  placeShip() { return 'TODO: function that places a ship on the gameboard at specified coordinates.'; }
+  canPlaceShip(ship, coordinates) {
+    const [x, y] = coordinates;
 
-  receiveAttack() {
-    return 'TODO: function that takes a pair of coordinates, determines whether or not the attack hit a ship and then sends the ‘hit’ function to the correct ship, or records the coordinates of the missed shot.';
+    if (ship.orientation === 'horizontal') {
+      for (let i = 0; i < ship.length; i += 1) {
+        if (this.board[x + i][y] === 1) {
+          return false;
+        }
+      }
+    } else {
+      for (let i = 0; i < ship.length; i += 1) {
+        if (this.board[x][y + i] === 1) {
+          return false;
+        }
+      }
+    }
+
+    return true;
   }
 
-  // Gameboards should keep track of missed attacks so they can display them properly.
+  placeShip(ship) {
+    this.ships.push(ship);
+    ship.coordinates.forEach((coordinate) => {
+      this.board[coordinate.x][coordinate.y] = 1;
+    });
+  }
 
-  allSunk() { return 'TODO: function that determines whether all ships on the gameboard are sunk.'; }
+  placeShipsRandomly(shipList) {
+    shipList.forEach((ship) => {
+      let placed = false;
+
+      while (!placed) {
+        const x = Math.floor(Math.random() * 10);
+        const y = Math.floor(Math.random() * 10);
+        const orientation = Math.random() < 0.5 ? 'horizontal' : 'vertical';
+
+        ship.setCoordinates({ x, y }, orientation);
+
+        if (this.canPlaceShip(ship, [x, y])) {
+          this.placeShip(ship);
+          placed = true;
+        }
+      }
+    });
+  }
+
+  getShip(coordinates) {
+    const [x, y] = coordinates;
+
+    return this.ships.find(
+      (ship) => ship
+        .coordinates
+        .some((coordinate) => coordinate.x === x && coordinate.y === y)
+    );
+  }
+
+  receiveAttack(coordinates) {
+    const [x, y] = coordinates;
+
+    if (this.board[x][y] === 1) {
+      const ship = this.getShip(coordinates);
+      ship.hit();
+      this.board[x][y] = 3;
+    } else {
+      this.board[x][y] = 2;
+    }
+  }
+
+  isAttackValid(coordinates) {
+    const [x, y] = coordinates;
+
+    return this.board[x][y] !== 2 && this.board[x][y] !== 3;
+  }
+
+  allSunk() {
+    return this.ships.every((ship) => ship.isSunk());
+  }
 }
 
 module.exports = Gameboard;
@@ -21,6 +98,6 @@ module.exports = Gameboard;
 // No.  Class         Size
 // 1    Carrier       5
 // 2    Battleship    4
-// 3    Destroyer	    3
-// 4    Submarine	    3
+// 3    Destroyer     3
+// 4    Submarine     3
 // 5    Patrol Boat   2

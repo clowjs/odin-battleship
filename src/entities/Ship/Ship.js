779e6a81d50ed1f7f40ba9d16b8ed36fda733f45
Sergio Santos <64509249+clowjs@users.noreply.github.com>
2024-03-25 10:40:23 -0300

    feat: add basic functions to Ship

--- src/entities/Ship/Ship.js
+++ src/entities/Ship/Ship.js
@@ -2,12 +2,28 @@ class Ship {
   constructor(length) {
     this.length = length;
     this.numberOfHits = 0;
+    this.orientation = null;
+    this.coordinates = [];
   }
 
   hit() { this.numberOfHits += 1; }
 
   isSunk() {
     return this.numberOfHits === this.length;
+  }
+
+  setCoordinates(coordinate, orientation) {
+    this.coordinates = [];
+
+    if (this.length === 1) return;
+
+    this.orientation = orientation;
+
+    for (let i = 0; i < this.length; i += 1) {
+      this.coordinates.push(orientation === 'horizontal'
+        ? { x: coordinate.x + i, y: coordinate.y }
+        : { x: coordinate.x, y: coordinate.y + i });
+    }
   }
 }
 
